37f349c3a95b9b5ebdc4637e7f339d723828b26c
VeraArk <veraarkind@gmail.com>
2024-07-25 14:29:22 +0300

    Lesson16

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Homework15 from "homework/Homework15/Homework15"
+import Homework15 from "homeworks/Homework15/Homework15"
+import Lesson16 from "lessons/Lesson16/Lesson16"
 
 function App() {
   return (
     <div>
-      <Homework15 />
+      {/* <Homework15 /> */}
+     <Lesson16 />
     </div>
   )
 }
